e0afce1bdc0f20412d1289582f9f5d5c52b9e055
Parth Sharma <parthsharma151@gmail.com>
2020-07-03 18:06:53 +0530

    h1 stable build for ledger

---
+++ main.js
@@ -0,0 +1,53 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { Menu } = require('electron');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// listen for the app to be ready
+app.on('ready', function() {
+
+    // creating a new window
+    mainWindow = new BrowserWindow({});
+
+    // load HTML into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // insert menu
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+
+// creating menu template
+const mainMenuTemplate = [
+    {
+        label:'Electron',
+    },
+    {
+        label:'File',
+        submenu: [
+            {
+                label: 'Add Item'
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
